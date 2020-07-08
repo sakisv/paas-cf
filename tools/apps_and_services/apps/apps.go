bbb8a5decd1b5cc2aa11e488f883b4d04d4bd79c
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2020-07-08 10:20:57 +0100

    Add app-binding calculation

--- tools/apps_and_services/apps/apps.go
+++ tools/apps_and_services/apps/apps.go
@@ -1,20 +1,23 @@
 package apps
 
 import (
+	"os"
+	"strings"
+	"time"
+
 	"github.com/alphagov/paas-cf/tools/apps_and_services/utils"
 	"github.com/cloudfoundry-community/go-cfclient"
 	"github.com/xenolf/lego/log"
-	"os"
-	"time"
 )
 
 type AppDetails struct {
-	Org			string 	`csv:"org"`
-	SpaceName	string 	`csv:"space_name"`
-	AppName    	string 	`csv:"app"`
-	AppID  		string 	`csv:"app_id"`
-	Bindings   	int 	`csv:"binding_count"`
-	Region 		string 	`csv:"region"`
+	Org       string `csv:"org"`
+	SpaceName string `csv:"space_name"`
+	AppName   string `csv:"app"`
+	AppID     string `csv:"app_id"`
+	Instances int    `csv:"instances"`
+	Bindings  int    `csv:"binding_count"`
+	Region    string `csv:"region"`
 }
 
 func FetchData(client Client, region string) []AppDetails {
@@ -35,32 +38,43 @@ func FetchData(client Client, region string) []AppDetails {
 			log.Fatal(err)
 			return nil
 		}
+		if strings.HasPrefix(app.Name, "SMOKES") || strings.HasPrefix(app.Name, "ACC") || strings.HasPrefix(app.Name, "BACC") || strings.HasPrefix(app.Name, "CAT") {
+			continue
+		}
 		status.Text(app.Name)
 		appdata = append(appdata, app)
-		appsummary , err := app.Summary()
+		serviceBindings, err := GetBindingsCount(app.Guid, client)
 		if err != nil {
 			log.Fatal(err)
 			return nil
 		}
-		appData , err:= client.GetAppByGuidNoInlineCall(app.Guid)
-		if err != nil {
-			log.Fatal(err)
-			return nil
-		}
-
-		spaceData , err:= client.GetSpaceByGuid(appData.SpaceGuid)
+		appData, err := client.GetAppByGuidNoInlineCall(app.Guid)
 		if err != nil {
 			log.Fatal(err)
 			return nil
 		}
 
-		orgData , err:= client.GetOrgByGuid(spaceData.OrganizationGuid)
+		spaceData, err := client.GetSpaceByGuid(appData.SpaceGuid)
 		if err != nil {
 			log.Fatal(err)
 			return nil
 		}
 
-		record := AppDetails{Org: orgData.Name , SpaceName: spaceData.Name, AppName: app.Name, AppID: app.Guid, Bindings: appsummary.ServiceCount, Region: region}
+		orgData, err := client.GetOrgByGuid(spaceData.OrganizationGuid)
+		if err != nil {
+			log.Fatal(err)
+			return nil
+		}
+
+		record := AppDetails{
+			Org:       orgData.Name,
+			SpaceName: spaceData.Name,
+			AppName:   app.Name,
+			AppID:     app.Guid,
+			Bindings:  serviceBindings,
+			Instances: app.Instances,
+			Region:    region,
+		}
 		data = append(data, record)
 
 		time.Sleep(5000)
--- tools/apps_and_services/apps/cf.go
+++ tools/apps_and_services/apps/cf.go
@@ -1,15 +1,23 @@
 package apps
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/cloudfoundry-community/go-cfclient"
+	"github.com/pkg/errors"
 )
 
 // This interface is extracted as a subset of
 // methods on the `Client` struct of
-// github.com/cloudfoundry-community/go-cfclent.
+// github.com/cloudfoundry-community/go-cfclient.
 // We use it so that we can mock the CF client
 // calls elsewhere.
 type Client interface {
+	NewRequest(method, path string) *cfclient.Request
+	DoRequest(req *cfclient.Request) (*http.Response, error)
 	ListApps() ([]cfclient.App, error)
 	GetAppByGuidNoInlineCall(guid string) (cfclient.App, error)
 	GetSpaceByGuid(spaceGUID string) (cfclient.Space, error)
@@ -18,4 +26,30 @@ type Client interface {
 
 type App interface {
 	Summary() ([]cfclient.AppSummary, error)
+}
+
+type ServiceBindings struct {
+	Pagination struct {
+		Results int `json:"total_results"`
+	} `json:"pagination"`
+}
+
+func GetBindingsCount(guid string, client Client) (int, error) {
+	var serviceBindings ServiceBindings
+	requestURL := fmt.Sprintf("/v3/service_bindings?app_guids=%s", guid)
+	r := client.NewRequest("GET", requestURL)
+	resp, err := client.DoRequest(r)
+	if err != nil {
+		return 0, errors.Wrap(err, "Error requesting app summary")
+	}
+	resBody, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return 0, errors.Wrap(err, "Error reading app summary body")
+	}
+	err = json.Unmarshal(resBody, &serviceBindings)
+	if err != nil {
+		return 0, errors.Wrap(err, "Error unmarshalling app summary")
+	}
+	return serviceBindings.Pagination.Results, nil
 }
