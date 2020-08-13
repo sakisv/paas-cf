30183525782b19fdd7e21cf4c3b223d5e944f2b7
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-08-13 17:31:10 +0100

    credhub: is externally accessible via gorouter
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- manifests/cf-manifest/spec/manifest/router_spec.rb
+++ manifests/cf-manifest/spec/manifest/router_spec.rb
@@ -6,5 +6,18 @@ RSpec.describe "router" do
     it "has max_idle_connections set to disable keepalives" do
       expect(gorouter_props.dig("max_idle_connections")).to eq(0)
     end
+
+    it "has ca_certs which is a superset of cf-deployment" do
+      cfd_ca_certs = cf_deployment_manifest
+        .dig("instance_groups")
+        .find { |ig| ig["name"] == "router" }
+        .dig("jobs")
+        .find { |ig| ig["name"] == "gorouter" }
+        .dig("properties", "router", "ca_certs")
+
+      ca_certs = gorouter_props.fetch("ca_certs")
+
+      expect(ca_certs).to include(cfd_ca_certs)
+    end
   end
 end
