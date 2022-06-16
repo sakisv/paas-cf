4eefbef9e214ff0871d25af2767d70a5750afa7d
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2022-06-16 13:58:30 +0100

    Maintain a metric for the number of applications as a Prometheus recording rule
    The performance data we present has previously used a very heavy query to count
    the number of applications at intervals in the last year, which included lots
    of aggregation. To improve the performance, we introduce a recording rule which
    counts the number of applications using the same aggregations, but on a smaller
    dataset, and saves the result as the metric "cf_app:count".

---
+++ manifests/prometheus/spec/recording_rules_spec.rb
@@ -0,0 +1,26 @@
+require "open3"
+
+RSpec.describe "prometheus recording rules" do
+  prom_alerts = nil
+  rules_file_location = "spec/alerts/fixtures/rules.yml"
+
+  before do
+    prom_alerts = YAML.dump(
+      "groups" =>
+        manifest_with_defaults["instance_groups"]
+          .select { |ig| ig["name"] == "prometheus2" }
+          .first["jobs"]
+          .select { |j| j["name"] == "prometheus2" }
+          .first.dig("properties", "prometheus", "custom_rules"),
+    )
+
+    File.write(rules_file_location, prom_alerts)
+  end
+
+  Dir.glob("*.test.yml", base: "spec/recording-rules/") do |filename|
+    it "runs #{filename}" do
+      _, err, status = Open3.capture3("promtool test rules spec/recording-rules/#{filename}")
+      expect(status.success?).to be(true), "`promtool test rules #{filename}` should pass. #{err}"
+    end
+  end
+end
