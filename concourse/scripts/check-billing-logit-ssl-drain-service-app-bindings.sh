71ba54465c9a7fbf4b221a88f3dc1a8238d34b5c
Jack Joy <jack.joy@digital.cabinet-office.gov.uk>
2022-10-28 15:46:02 +0100

    added check-billing-logit-ssl-drain-service-app-bindings.sh to replace cf grep

--- concourse/scripts/check-billing-logit-ssl-drain-service-app-bindings.sh
+++ concourse/scripts/check-billing-logit-ssl-drain-service-app-bindings.sh
@@ -1,34 +1,38 @@
 #!/usr/bin/env bash
 
-APP1='paas-billing-api'
-APP2='paas-billing-collector'
+# This script checks that bindings exist between the service billing-logit-ssl-drain and the billing apps. This is to ensure that the billing apps have been deployed successfully.
 
-APP1_GUID=$(cf curl "/v3/apps?names=${APP1}" | jq -r '.resources[].guid')
-APP2_GUID=$(cf curl "/v3/apps?names=${APP2}" | jq -r '.resources[].guid')
+set -e -u -o pipefail
 
-SI_GUID=$(cf curl /v3/service_instances | jq -r '.resources[]|select(.name=="billing-logit-ssl-drain").guid')
+APP1_NAME='paas-billing-api'
+APP2_NAME='paas-billing-collector'
 
-SCB=$(cf curl "/v3/service_credential_bindings?service_instance_guids=${SI_GUID}")
+APP1_GUID=$(cf curl "/v3/apps?names=${APP1_NAME}" | jq -r '.resources[].guid')
+APP2_GUID=$(cf curl "/v3/apps?names=${APP2_NAME}" | jq -r '.resources[].guid')
 
-SCB_COUNT=$(echo "$SCB" | jq -r '.resources|length')
-if [ "$SCB_COUNT" != "2" ]; then
-  echo "ERROR: expecting 2 service credential bindings. got: $SCB_COUNT"
+SERVICE_INSTANCE_GUID=$(cf curl /v3/service_instances | jq -r '.resources[]|select(.name=="billing-logit-ssl-drain").guid')
+
+SERVICE_CREDENTIAL_BINDINGS=$(cf curl "/v3/service_credential_bindings?service_instance_guids=${SERVICE_INSTANCE_GUID}")
+
+SERVICE_CREDENTIAL_BINDINGS_COUNT=$(jq -r '.resources|length' <<< "${SERVICE_CREDENTIAL_BINDINGS}")
+if [[ "${SERVICE_CREDENTIAL_BINDINGS_COUNT}" != "2" ]]; then
+  echo "ERROR: expecting 2 service credential bindings. got: ${SERVICE_CREDENTIAL_BINDINGS_COUNT}"
   exit 1
 fi
 
-BIND1=$(jq -r '.resources[0].relationships.app.data.guid' <<< "${SCB}")
-STATE1=$(jq -r '.resources[0].last_operation.state' <<< "${SCB}")
-TYPE1=$(jq -r '.resources[0].last_operation.type' <<< "${SCB}")
-BIND2=$(jq -r '.resources[1].relationships.app.data.guid' <<< "${SCB}")
-STATE2=$(jq -r '.resources[1].last_operation.state' <<< "${SCB}")
-TYPE2=$(jq -r '.resources[1].last_operation.type' <<< "${SCB}")
+BIND1_GUID=$(jq -r '.resources[0].relationships.app.data.guid' <<< "${SERVICE_CREDENTIAL_BINDINGS}")
+BIND1_STATE=$(jq -r '.resources[0].last_operation.state' <<< "${SERVICE_CREDENTIAL_BINDINGS}")
+BIND1_TYPE=$(jq -r '.resources[0].last_operation.type' <<< "${SERVICE_CREDENTIAL_BINDINGS}")
+BIND2_GUID=$(jq -r '.resources[1].relationships.app.data.guid' <<< "${SERVICE_CREDENTIAL_BINDINGS}")
+BIND2_STATE=$(jq -r '.resources[1].last_operation.state' <<< "${SERVICE_CREDENTIAL_BINDINGS}")
+BIND2_TYPE=$(jq -r '.resources[1].last_operation.type' <<< "${SERVICE_CREDENTIAL_BINDINGS}")
 
-if ! [[ "${BIND1}" = "${APP1_GUID}" && "${BIND2}" = "${APP2_GUID}" || "${BIND1}" = "${APP2_GUID}" && "${BIND2}" = "${APP1_GUID}" ]]; then
-  echo "ERROR: could not match app guis to service binds"
+if ! [[ "${BIND1_GUID}" = "${APP1_GUID}" && "${BIND2_GUID}" = "${APP2_GUID}" || "${BIND1_GUID}" = "${APP2_GUID}" && "${BIND2_GUID}" = "${APP1_GUID}" ]]; then
+  echo "ERROR: could not match app GUIDS to service binds"
   exit 1
 fi
 
-if ! [[ "$STATE1" = "succeeded" && "$TYPE1" = "create" && "$STATE2" = "succeeded" && "$TYPE2" = "create" ]]; then
+if ! [[ "${BIND1_STATE}" = "succeeded" && "${BIND1_TYPE}" = "create" && "${BIND2_STATE}" = "succeeded" && "${BIND2_TYPE}" = "create" ]]; then
   echo "ERROR: one or more bad bind states/types"
   exit 1
 fi
